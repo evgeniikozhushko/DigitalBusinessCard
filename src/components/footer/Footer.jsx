4abd9c86ae8e73da5f1e2867d5ba613bf4bb5693
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-07-07 21:57:23 -0600

    add icons, test Netlify for errors

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-// import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from "react-icons/fa"
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from "react-icons/fa"
 import './Footer.css'
 
 export default function Footer() {
     return (
         <div className="footer--container">
             <div className="icons--container">
-                {/* <a href="https://twitter.com/EvgeniiKremlev" target="_blank"><FaTwitterSquare className='social--icons'/></a>
+                <a href="https://twitter.com/EvgeniiKremlev" target="_blank"><FaTwitterSquare className='social--icons'/></a>
                 <a href="https://www.facebook.com/profile.php?id=100070864575959" target="_blank"><FaFacebookSquare className='social--icons'/></a>
                 <a href="https://www.instagram.com/evgenii.ca/" target="_blank"><FaInstagramSquare className='social--icons'/></a>
-                <a href="https://github.com/evgeniikozhushko" target="_blank"><FaGithubSquare className='social--icons'/></a> */}
+                <a href="https://github.com/evgeniikozhushko" target="_blank"><FaGithubSquare className='social--icons'/></a>
             </div>
         </div>
     )
--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { FaEnvelope, FaLinkedin } from "react-icons/fa"
+import { FaEnvelope, FaLinkedin } from "react-icons/fa"
 import infoImg from '../../assets/2023_EK.jpeg'
 import './Info.css'
 
@@ -12,12 +12,12 @@ export default function Info() {
             <a href="https://evgenii.ca/" target="_blank"><h6>evgenii.ca</h6></a>
             <div className="info--button--container">
                 <a href="mailto:info@evgenii.ca" target="_blank">
-                {/* <button className="btn--info--email"><FaEnvelope className='icon--email'/> Email</button> */}
-                <button className="btn--info--email">Email</button>
+                <button className="btn--info--email"><FaEnvelope className='icon--email'/> Email</button>
+                {/* <button className="btn--info--email">Email</button> */}
                 </a>
                 <a href="https://www.linkedin.com/in/evgenii-kozhushko/" target="_blank">
-                {/* <button className="btn--info--linkedin"><FaLinkedin className='icon--linkedin'/>LinkedIn</button> */}
-                <button className="btn--info--linkedin">LinkedIn</button>
+                <button className="btn--info--linkedin"><FaLinkedin className='icon--linkedin'/>LinkedIn</button>
+                {/* <button className="btn--info--linkedin">LinkedIn</button> */}
                 </a>
             </div>
         </div>
